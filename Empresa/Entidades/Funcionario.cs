1fb5054feb36d4bd706cbea4d8e1b0dcc57235ee
Vinícius Silva <vinisilva45@live.com>
2020-09-08 14:38:20 -0300

    Adicionar arquivos de projeto.

---
+++ Empresa/Entidades/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Empresa.Entidades
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public int Horas { get; set; }
+
+        public double ValorHora { get; set; }
+
+        public Funcionario() 
+        {
+        }
+
+        public Funcionario(string nome, int horas, double valorHora)
+        {
+            Nome = nome;
+            Horas = horas;
+            ValorHora = valorHora;
+        }
+
+        public double Pagamento()
+        {
+            return Horas * ValorHora;
+        }
+    }   
+}
